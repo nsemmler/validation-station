dc561d78b136680b852ff8c4125c93dab7db77f9
Nate <ndsunc@gmail.com>
2018-05-30 11:37:02 -0700

    Tackled every task in README, with notes in index.html and main.js files

--- readme.md
+++ readme.md
@@ -12,11 +12,11 @@ An example repository for learning client side validation.
 
 It is suggested you tackle each task in the given order.
 
-- [ ] Update the `<script>` tag so that we are able to correctly select the form.
-- [ ] Hijack the form so that the default action does not happen when you press the submit button.
-- [ ] Require both the **Kind** and **Brand** fields.
-- [ ] Limit the "Number of Seats" to be above 0.
-- [ ] If the user selects a "Passenger" or "Observation" car, require there to be a value in "Number of Seats".
-- [ ] If there is an error, surface those errors to the user.
-- [ ] If there is not an error, clear the values from the form and show a success message.
-- [ ] If there is not an error, add a new list item to the "Train Cars" column with information about the car.
+- [x] Update the `<script>` tag so that we are able to correctly select the form. (could also do DOMContentLoaded event listener in JS)
+- [x] Hijack the form so that the default action does not happen when you press the submit button.
+- [x] Require both the **Kind** and **Brand** fields.
+- [x] Limit the "Number of Seats" to be above 0.
+- [x] If the user selects a "Passenger" or "Observation" car, require there to be a value in "Number of Seats".
+- [x] If there is an error, surface those errors to the user.
+- [x] If there is not an error, clear the values from the form and show a success message.
+- [x] If there is not an error, add a new list item to the "Train Cars" column with information about the car.
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,35 @@
 console.log('Welcome to Validation Station!')
-console.log('NEW TRAIN CAR FORM:', document.querySelector('#new-train-car-form'))
+
+const form = document.querySelector('#new-train-car-form')
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const kind = document.querySelector('#train-kind') // could also do: kind = event.target.kind (b/c name="kind" in <select>)
+  const error = document.querySelector('#error') // <div> containing the empty <p>
+  if (kind.value === 'observation' || kind.value === 'passenger') {
+    const seats = event.target.seats
+    if (!seats.value) {
+      error.querySelector('p').textContent = 'You must have a number of seats.'
+      error.classList.remove('d-none') // remove from classList
+    }
+  }
+
+  if (error.classList.contains('d-none')) {
+    error.classList.remove('alert-danger')
+    error.classList.add('alert-success')
+    error.querySelector('p').textContent = 'Success.'
+    error.classList.remove('d-none')
+    addCarToList(event)
+    form.reset() // reset AFTER adding car to list - otherwise you are deleting the values from the form before you save it as new car
+  }
+})
+
+function addCarToList (event) {
+  const carList = document.querySelector('#car-list')
+  const brand = event.target.brand
+  const kind = event.target.kind
+  const numSeats = event.target.seats
+  const newCar = `<li>
+                    <p>${brand.value} ${kind.value} (${numSeats.value})</p>
+                  </li>`
+  carList.innerHTML += newCar
+}
